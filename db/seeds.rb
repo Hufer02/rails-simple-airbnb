ecf1e6f3ccded58f304c0d9a524107bb0ec0cba4
Hugo Ferrier <hu.ferrier@gmail.com>
2024-05-09 21:28:40 +0200

    started working on the airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+
+appartment1= Flat.create!(name: Faker::Fantasy::Tolkien.character, address: Faker::Address.full_address, description: Faker::Fantasy::Tolkien.poem, price_per_night: rand(50..300), number_of_guests: rand(20))
+appartment2= Flat.create!(name: Faker::Fantasy::Tolkien.character, address: Faker::Address.full_address, description: Faker::Fantasy::Tolkien.poem, price_per_night: rand(50..300), number_of_guests: rand(20))
+appartment3= Flat.create!(name: Faker::Fantasy::Tolkien.character, address: Faker::Address.full_address, description: Faker::Fantasy::Tolkien.poem, price_per_night: rand(50..300), number_of_guests: rand(20))
+appartment4= Flat.create!(name: Faker::Fantasy::Tolkien.character, address: Faker::Address.full_address, description: Faker::Fantasy::Tolkien.poem, price_per_night: rand(50..300), number_of_guests: rand(20))
+appartment5= Flat.create!(name: Faker::Fantasy::Tolkien.character, address: Faker::Address.full_address, description: Faker::Fantasy::Tolkien.poem, price_per_night: rand(50..300), number_of_guests: rand(20))
+
+puts "Finished!"
